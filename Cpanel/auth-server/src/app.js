4bc4fde0d33ce411145ea3122ddabcbb9e8bef80
PRANESH KARTHI M S <107921577+mspk5196@users.noreply.github.com>
2025-12-01 20:42:57 +0530

    modified env of backend

--- Cpanel/auth-server/src/app.js
+++ Cpanel/auth-server/src/app.js
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors(corsConfig));
+// app.use(cors(corsConfig));
 // Explicit preflight handler reusing same config
 app.options(/.*/, cors(corsConfig));
 app.use(express.json());

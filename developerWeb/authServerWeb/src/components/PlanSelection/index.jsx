a92ad7270d277c2d7109f9afa2b8bd076e89c363
PRANESH KARTHI M S <107921577+mspk5196@users.noreply.github.com>
2025-12-25 20:59:42 +0530

    modified docker file

--- developerWeb/authServerWeb/src/components/PlanSelection/index.jsx
+++ developerWeb/authServerWeb/src/components/PlanSelection/index.jsx
@@ -3,6 +3,7 @@ import { api } from '../../utils/api';
 import paymentService from '../../services/paymentService';
 import { getFeatureSentences } from '../PlanFeatures/PlanFeatures';
 import './PlanSelection.scss';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const PlanSelection = ({ onPlanSelected, currentPlanId }) => {
   const [plans, setPlans] = useState([]);
@@ -19,6 +20,8 @@ const PlanSelection = ({ onPlanSelected, currentPlanId }) => {
   const fetchPlans = async () => {
     try {
       setLoading(true);
+      console.log(API_BASE_URL);
+      
       const response = await api.get('/developer/plans');
       const fetched = response.data.plans || [];
       setPlans(fetched);
